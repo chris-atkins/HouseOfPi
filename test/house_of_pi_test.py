43e2427328810be9901d3b7d21576e2572e91d74
Chris Atkins <chrisatkins55@gmail.com>
2016-02-28 20:41:41 -0500

    another test method for HouseOfPi class - starting to figure out mocking a little more

--- test/house_of_pi_test.py
+++ test/house_of_pi_test.py
@@ -2,16 +2,27 @@
 from app.hardware.house_of_pi import HouseOfPi
 import unittest
 from mock import MagicMock
-import time
+from mock import patch
 
 class HouseOfPiTest(unittest.TestCase):
 
-    def test_on_called_one_time(self):
-        mock_gpio_factory = MagicMock()
-        mock_gpio = MagicMock()
-        mock_gpio_factory.getGPIO.return_value = mock_gpio
+    def setUp(self):
+        self.mock_gpio_factory = MagicMock()
+        self.mock_gpio = self.mock_gpio_factory.getGPIO.return_value
+    
+    
+    @patch('app.hardware.house_of_pi.TimedLEDDisplay')
+    def test_start_displaying_on_state_is_wired_correctly(self, mock_LED_display):
+        mock_LED_display_instance = mock_LED_display.return_value
+        pi = HouseOfPi(self.mock_gpio_factory)
+        pi.start_displaying_on_state(42)
         
-        pi = HouseOfPi(mock_gpio_factory)
+        mock_LED_display.assert_called_with(self.mock_gpio, 42)
+        mock_LED_display_instance.start_led_display_every.assert_called_with(pi.led_display_cycle_time)
+
+
+    def test_on_called_one_time(self):
+        pi = HouseOfPi(self.mock_gpio_factory)
         pi.blink_n_times_in_time(1, 1)
-        self.assertTrue(mock_gpio.output.called)
+        self.assertTrue(self.mock_gpio.output.called)
     
