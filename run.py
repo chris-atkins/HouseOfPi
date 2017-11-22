5883d1b92c2563c55d2fe5f1b1592518f811f73c
Chris Atkins <chrisatkins55@gmail.com>
2017-11-21 23:50:04 -0500

    fixing server name for hue lights since comcast seems to not honor the hostname

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ app = Server(__name__, hardware)
 app.config.update(dict(
     MY_HOUSE_URL='https://poorknight.com',
     THERMOSTAT_URL='http://thermostat-76-B6-35',
-    LIGHTS_URL='http://Philips-hue',
+    LIGHTS_URL='http://10.0.0.196',
     AUTHENTICATION_SECRET=os.environ.get('AUTHENTICATION_SECRET')
 ))
 
