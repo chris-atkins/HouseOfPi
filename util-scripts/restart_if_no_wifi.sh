e5c0590004d789a68fc6dbc42d997c069e8cd441
Chris <chrisatkins55@gmail.com>
2019-06-29 13:27:09 -0400

    Add timeout for ping when checking wifi status and rebooting if N/A

--- util-scripts/restart_if_no_wifi.sh
+++ util-scripts/restart_if_no_wifi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ping -c 1 www.google.com
+ping -c 1 -W 1 www.google.com
 rc=$?
 if [[ $rc -ne 0 ]] ; then
     echo "$(date) Not able to access internet - restarting"  >> /home/pi/wifi-check.log
